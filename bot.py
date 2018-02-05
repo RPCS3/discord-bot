fed583673c0f8f9b973fd61086302ee3f1f12d45
Nicba1010 <nicba1010@gmail.com>
2018-02-05 02:00:19 +0100

    Stupid mistake

--- bot.py
+++ bot.py
@@ -184,7 +184,7 @@ async def get_code(code: str) -> object:
     :return: data or None
     """
     result = ApiRequest().set_search(code).set_amount(10).request()
-    if len(result.results) == 1:
+    if len(result.results) >= 1:
         for result in result.results:
             if result.game_id == code:
                 return "```" + result.to_string() + "```"
