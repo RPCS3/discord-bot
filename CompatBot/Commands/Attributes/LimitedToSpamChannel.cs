cb1670ccca31d245b7de257989aae0a1ee0f89aa
13xforever <ilya.veselov@gmail.com>
2018-08-06 01:41:02 +0500

    send a message explaining that the command is restricted to a specific channel

--- CompatBot/Commands/Attributes/LimitedToSpamChannel.cs
+++ CompatBot/Commands/Attributes/LimitedToSpamChannel.cs
@@ -8,15 +8,16 @@ namespace CompatBot.Commands.Attributes
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false)]
     internal class LimitedToSpamChannel: CheckBaseAttribute
     {
-        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        public override async Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
         {
             if (ctx.Channel.IsPrivate || help)
-                return Task.FromResult(true);
+                return true;
 
             if (ctx.Channel.Name.Contains("spam", StringComparison.InvariantCultureIgnoreCase))
-                return Task.FromResult(true);
+                return true;
 
-            return Task.FromResult(false);
+            await ctx.RespondAsync($"`{Config.CommandPrefix}{ctx.Command.QualifiedName}` is limited to bot spam channel and DMs").ConfigureAwait(false);
+            return false;
         }
     }
 }
