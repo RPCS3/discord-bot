295dda9da25d64b728fec15e7e8511e405bd9c67
13xforever <ilya.veselov@gmail.com>
2018-08-17 21:12:01 +0500

    new command to scrape psn by full content id; useful for delisted games

--- CompatBot/Commands/Psn.Check.cs
+++ CompatBot/Commands/Psn.Check.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using CompatApiClient.Utils;
 using CompatBot.EventHandlers;
+using CompatBot.ThumbScrapper;
 using CompatBot.Utils;
 using CompatBot.Utils.ResultFormatters;
 using DSharpPlus.CommandsNext;
@@ -16,11 +17,11 @@ namespace CompatBot.Commands
 
         [Group("check")]
         [Description("Commands to check for various stuff on PSN")]
-        public sealed class Check : BaseCommandModuleCustom
+        public sealed class Check: BaseCommandModuleCustom
         {
             [Command("updates"), Aliases("update")]
             [Description("Checks if specified product has any updates")]
-            public async Task Updates(CommandContext ctx, [RemainingText, Description("Product ID such as BLUS12345")] string productId)
+            public async Task Updates(CommandContext ctx, [RemainingText, Description("Product ID such as `BLUS12345`")] string productId)
             {
                 productId = ProductCodeLookup.GetProductIds(productId).FirstOrDefault();
                 if (string.IsNullOrEmpty(productId))
@@ -31,8 +32,32 @@ namespace CompatBot.Commands
 
                 var updateInfo = await Client.GetTitleUpdatesAsync(productId, Config.Cts.Token).ConfigureAwait(false);
                 var embeds = await updateInfo.AsEmbedAsync(ctx.Client, productId).ConfigureAwait(false);
-                foreach(var embed in embeds)
+                foreach (var embed in embeds)
                     await ctx.RespondAsync(embed: embed).ConfigureAwait(false);
+            }
+
+            [Command("content")]
+            [Description("Adds PSN content id to the scraping queue")]
+            public async Task Content(CommandContext ctx, [RemainingText, Description("Content IDs to scrape, such as `UP0006-NPUB30592_00-MONOPOLYPSNNA000`")] string contentIds)
+            {
+                if (string.IsNullOrEmpty(contentIds))
+                {
+                    await ctx.ReactWithAsync(Config.Reactions.Failure, "No IDs were specified").ConfigureAwait(false);
+                    return;
+                }
+
+                var matches = PsnScraper.ContentIdMatcher.Matches(contentIds);
+                var itemsToCheck = matches.Select(m => m.Groups["content_id"].Value).ToList();
+                if (itemsToCheck.Count == 0)
+                {
+                    await ctx.ReactWithAsync(Config.Reactions.Failure, "No IDs were specified").ConfigureAwait(false);
+                    return;
+                }
+
+                foreach (var id in itemsToCheck)
+                    PsnScraper.CheckContentId(id, Config.Cts.Token);
+
+                await ctx.ReactWithAsync(Config.Reactions.Success, $"Added {itemsToCheck.Count} ID{StringUtils.GetSuffix(itemsToCheck.Count)} to the scraping queue").ConfigureAwait(false);
             }
         }
     }
