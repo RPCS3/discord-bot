23d407155532df892fa28aac05929fd28a07ea6b
13xforever <ilya.veselov@gmail.com>
2018-09-10 18:58:44 +0500

    !report message

---
+++ CompatBot/Commands/Attributes/RequiresWhitelistedRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using CompatBot.Utils;
+using DSharpPlus.CommandsNext;
+
+namespace CompatBot.Commands.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false)]
+    internal class RequiresWhitelistedRole: CheckBaseAttributeWithReactions
+    {
+        public RequiresWhitelistedRole() : base(reactOnFailure: Config.Reactions.Denied) { }
+
+        protected override Task<bool> IsAllowed(CommandContext ctx, bool help)
+        {
+            return Task.FromResult(ctx.User.IsWhitelisted(ctx.Client, ctx.Guild));
+        }
+    }
+}
