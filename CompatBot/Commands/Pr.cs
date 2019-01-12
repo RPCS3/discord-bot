220f8e0af3628d5160a8a598e19878dba8b97d14
13xforever <ilya.veselov@gmail.com>
2019-01-13 00:53:54 +0500

    bump new master build time to 2*20, as there's pr build before it

--- CompatBot/Commands/Pr.cs
+++ CompatBot/Commands/Pr.cs
@@ -22,7 +22,7 @@ namespace CompatBot.Commands
         private static readonly GithubClient.Client githubClient = new GithubClient.Client();
         private static readonly AppveyorClient.Client appveyorClient = new AppveyorClient.Client();
         private static readonly CompatApiClient.Client compatApiClient = new CompatApiClient.Client();
-        private static readonly TimeSpan AvgBuildTime = TimeSpan.FromMinutes(20);
+        private static readonly TimeSpan AvgBuildTime = TimeSpan.FromMinutes(40); // it's 20, but on merge we have pr + master builds
         private const string appveyorContext = "continuous-integration/appveyor/pr";
 
         [GroupCommand]
