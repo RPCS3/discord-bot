f56b5332a325b6275ca3cff37a6f2bb37b4d4052
13xforever <ilya.veselov@gmail.com>
2019-01-30 04:54:15 +0500

    try to fix !warn list for some users

--- CompatBot/Commands/Warnings.cs
+++ CompatBot/Commands/Warnings.cs
@@ -135,48 +135,59 @@ namespace CompatBot.Commands
         //note: be sure to pass a sanitized userName
         private static async Task ListUserWarningsAsync(DiscordClient client, DiscordMessage message, ulong userId, string userName, bool skipIfOne = true)
         {
-            var channel = message.Channel;
-            int count;
-            using (var db = new BotDb())
-                count = await db.Warning.CountAsync(w => w.DiscordId == userId).ConfigureAwait(false);
-            if (count == 0)
+            try
             {
-                await message.RespondAsync(userName + " has no warnings, is a standup citizen, and a pillar of this community").ConfigureAwait(false);
-                return;
-            }
-
-            if (count == 1 && skipIfOne)
-                return;
-
-            const int maxWarningsInPublicChannel = 3;
-            var isPrivate = channel.IsPrivate;
-            var isWhitelisted = client.GetMember(message.Author).IsWhitelisted();
-            using (var db = new BotDb())
-            {
-                var totalWarningCount = db.Warning.Count(w => w.DiscordId == userId);
-                var showCount = Math.Min(maxWarningsInPublicChannel, totalWarningCount);
-                var result = new StringBuilder("Warning list for ").Append(userName);
-                if (!isPrivate && !isWhitelisted && totalWarningCount > maxWarningsInPublicChannel)
-                    result.Append($" (last {showCount} of {totalWarningCount}, full list in DMs)");
-                result.AppendLine(":").AppendLine("```");
-                var header = $"{"ID",-5} | {"Issued by",-15} | {"On date (UTC)",-20} | Reason";
-                if (isPrivate)
-                    header += "          | Full reason";
-                result.AppendLine(header)
-                    .AppendLine("".PadLeft(header.Length, '-'));
-                IQueryable<Warning> query = db.Warning.Where(w => w.DiscordId == userId).OrderByDescending(w => w.Id);
-                if (!isPrivate && !isWhitelisted)
-                    query = query.Take(maxWarningsInPublicChannel);
-                foreach (var warning in query)
+                var channel = message.Channel;
+                int count;
+                using (var db = new BotDb())
+                    count = await db.Warning.CountAsync(w => w.DiscordId == userId).ConfigureAwait(false);
+                if (count == 0)
                 {
-                    var issuerName = warning.IssuerId == 0 ? "" : await client.GetUserNameAsync(channel, warning.IssuerId, isPrivate, "unknown mod").ConfigureAwait(false);
-                    var timestamp = warning.Timestamp.HasValue ? new DateTime(warning.Timestamp.Value, DateTimeKind.Utc).ToString("u") : null;
-                    result.Append($"{warning.Id:00000} | {issuerName,-15} | {timestamp,-20} | {warning.Reason}");
-                    if (isPrivate)
-                        result.Append(" | ").Append(warning.FullReason);
-                    result.AppendLine();
+                    await message.RespondAsync(userName + " has no warnings, is a standup citizen, and a pillar of this community").ConfigureAwait(false);
+                    return;
                 }
-                await channel.SendAutosplitMessageAsync(result.Append("```")).ConfigureAwait(false);
+
+                if (count == 1 && skipIfOne)
+                    return;
+
+                const int maxWarningsInPublicChannel = 3;
+                var isPrivate = channel.IsPrivate;
+                var isWhitelisted = client.GetMember(message.Author)?.IsWhitelisted() ?? false;
+                using (var db = new BotDb())
+                {
+                    var totalWarningCount = db.Warning.Count(w => w.DiscordId == userId);
+                    var showCount = Math.Min(maxWarningsInPublicChannel, totalWarningCount);
+                    var result = new StringBuilder("Warning list for ").Append(userName);
+                    if (!isPrivate && !isWhitelisted && totalWarningCount > maxWarningsInPublicChannel)
+                        result.Append($" (last {showCount} of {totalWarningCount}, full list in DMs)");
+                    result.AppendLine(":").AppendLine("```");
+                    var header = $"{"ID",-5} | {"Issued by",-15} | {"On date (UTC)",-20} | Reason";
+                    if (isPrivate)
+                        header += "          | Full reason";
+                    result.AppendLine(header)
+                        .AppendLine("".PadLeft(header.Length, '-'));
+                    IQueryable<Warning> query = db.Warning.Where(w => w.DiscordId == userId).OrderByDescending(w => w.Id);
+                    if (!isPrivate && !isWhitelisted)
+                        query = query.Take(maxWarningsInPublicChannel);
+                    foreach (var warning in await query.ToListAsync().ConfigureAwait(false))
+                    {
+                        var issuerName = warning.IssuerId == 0
+                            ? ""
+                            : await client.GetUserNameAsync(channel, warning.IssuerId, isPrivate, "unknown mod").ConfigureAwait(false);
+                        var timestamp = warning.Timestamp.HasValue
+                            ? new DateTime(warning.Timestamp.Value, DateTimeKind.Utc).ToString("u")
+                            : null;
+                        result.Append($"{warning.Id:00000} | {issuerName,-15} | {timestamp,-20} | {warning.Reason}");
+                        if (isPrivate)
+                            result.Append(" | ").Append(warning.FullReason);
+                        result.AppendLine();
+                    }
+                    await channel.SendAutosplitMessageAsync(result.Append("```")).ConfigureAwait(false);
+                }
+            }
+            catch (Exception e)
+            {
+                Config.Log.Warn(e);
             }
         }
     }
