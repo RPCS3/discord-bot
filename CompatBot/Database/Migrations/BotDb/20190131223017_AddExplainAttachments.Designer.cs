d5003e41b8e239c7436a2a79b6702e81b7bf1c22
13xforever <ilya.veselov@gmail.com>
2019-02-01 04:31:06 +0500

    add support for images/attachments in explanations

--- CompatBot/Database/Migrations/BotDb/20190131223017_AddExplainAttachments.Designer.cs
+++ CompatBot/Database/Migrations/BotDb/20190131223017_AddExplainAttachments.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CompatBot.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CompatBot.Database.Migrations
 {
     [DbContext(typeof(BotDb))]
-    partial class BotDbModelSnapshot : ModelSnapshot
+    [Migration("20190131223017_AddExplainAttachments")]
+    partial class AddExplainAttachments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CompatBot/Database/Migrations/BotDb/20190131223017_AddExplainAttachments.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CompatBot.Database.Migrations
+{
+    public partial class AddExplainAttachments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "attachment",
+                table: "explanation",
+                maxLength: 7340032,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "attachment_filename",
+                table: "explanation",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "attachment",
+                table: "explanation");
+
+            migrationBuilder.DropColumn(
+                name: "attachment_filename",
+                table: "explanation");
+        }
+    }
+}
