8895d9679d5caa859082ed2a1aed4434b95c24a2
13xforever <ilya.veselov@gmail.com>
2019-03-06 15:18:12 +0500

    more stats

---
+++ CompatBot/Utils/SandboxDetector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CompatBot.Utils
+{
+    public static class SandboxDetector
+    {
+        public static string Detect()
+        {
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SNAP")))
+                return "Snap";
+
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("FLATPAK_SYSTEM_DIR")))
+                return "Flatpak";
+
+            return null;
+        }
+    }
+}
