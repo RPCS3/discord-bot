a0f6edd97c3001dcf05bfce4178ef6832e105528
13xforever <ilya.veselov@gmail.com>
2019-08-07 21:53:15 +0500

    fix several issues with the autosplit messages

--- CompatBot/Utils/Extensions/AutosplitResponseHelper.cs
+++ CompatBot/Utils/Extensions/AutosplitResponseHelper.cs
@@ -10,17 +10,17 @@ namespace CompatBot.Utils
 {
     public static class AutosplitResponseHelper
     {
-        public static Task SendAutosplitMessageAsync(this CommandContext ctx, StringBuilder message, int blockSize = 2000, string blockEnd = "```", string blockStart = "```")
+        public static Task SendAutosplitMessageAsync(this CommandContext ctx, StringBuilder message, int blockSize = 2000, string blockEnd = "\n```", string blockStart = "```\n")
         {
             return ctx.Channel.SendAutosplitMessageAsync(message, blockSize, blockEnd, blockStart);
         }
 
-        public static Task SendAutosplitMessageAsync(this CommandContext ctx, string message, int blockSize = 2000, string blockEnd = "```", string blockStart = "```")
+        public static Task SendAutosplitMessageAsync(this CommandContext ctx, string message, int blockSize = 2000, string blockEnd = "\n```", string blockStart = "```\n")
         {
             return ctx.Channel.SendAutosplitMessageAsync(message, blockSize, blockEnd, blockStart);
         }
 
-        public static async Task SendAutosplitMessageAsync(this DiscordChannel channel, StringBuilder message, int blockSize = 2000, string blockEnd = "```", string blockStart = "```")
+        public static async Task SendAutosplitMessageAsync(this DiscordChannel channel, StringBuilder message, int blockSize = 2000, string blockEnd = "\n```", string blockStart = "```\n")
         {
             if (message == null)
                 throw new ArgumentNullException(nameof(message));
@@ -28,7 +28,7 @@ namespace CompatBot.Utils
             await SendAutosplitMessageAsync(channel, message.ToString(), blockSize, blockEnd, blockStart).ConfigureAwait(false);
         }
 
-        public static async Task SendAutosplitMessageAsync(this DiscordChannel channel, string message, int blockSize = 2000, string blockEnd = "```", string blockStart = "```")
+        public static async Task SendAutosplitMessageAsync(this DiscordChannel channel, string message, int blockSize = 2000, string blockEnd = "\n```", string blockStart = "```\n")
         {
             if (channel == null)
                 throw new ArgumentNullException(nameof(channel));
@@ -46,15 +46,32 @@ namespace CompatBot.Utils
                 if (buffer.Length + line.Length + blockEnd.Length > blockSize)
                 {
                     var content = buffer.ToString().Trim(blockSize - blockEnd.Length) + blockEnd;
-                    await channel.SendMessageAsync(content).ConfigureAwait(false);
+                    if (content.Length > blockSize)
+                        Config.Log.Error($"Somehow managed to go over {blockSize} characters in a message");
+                    try
+                    {
+                        await channel.SendMessageAsync(content).ConfigureAwait(false);
+                    }
+                    catch (Exception e)
+                    {
+                        Config.Log.Warn(e, "And yet the message length was " + content.Length);
+                    }
                     await channel.TriggerTypingAsync().ConfigureAwait(false);
                     buffer.Clear().Append(blockStart);
                 }
                 else
-                    buffer.AppendLine();
+                    buffer.Append('\n');
                 buffer.Append(line);
             }
-            await channel.SendMessageAsync(buffer.ToString()).ConfigureAwait(false);
+            var remainingContent = buffer.ToString().Trim(blockSize);
+            try
+            {
+                await channel.SendMessageAsync(remainingContent).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                Config.Log.Warn(e, "And yet the message length was " + remainingContent.Length);
+            }
         }
     }
 }
