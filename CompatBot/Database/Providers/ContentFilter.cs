bd0433038c8a491dfcdf32983e35581e5aba0c76
13xforever <ilya.veselov@gmail.com>
2019-11-25 21:33:19 +0500

    log removed message info

--- CompatBot/Database/Providers/ContentFilter.cs
+++ CompatBot/Database/Providers/ContentFilter.cs
@@ -118,6 +118,8 @@ namespace CompatBot.Database.Providers
                 try
                 {
                     await message.Channel.DeleteMessageAsync(message, $"Removed according to filter '{trigger}'").ConfigureAwait(false);
+                    var author = client.GetMember(message.Author);
+                    Config.Log.Debug($"Removed message from {author.GetMentionWithNickname()} in #{message.Channel.Name}: {message.Content}");
                     completedActions.Add(FilterAction.RemoveContent);
                 }
                 catch
