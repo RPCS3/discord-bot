6b27d2dd96bba6598fc2e6a177e206c17d794c24
13xforever <ilya.veselov@gmail.com>
2020-01-11 16:25:26 +0500

    minor text adjustment

--- CompatBot/Utils/ResultFormatters/LogParserResultFormatter.WeirdSettingsSection.cs
+++ CompatBot/Utils/ResultFormatters/LogParserResultFormatter.WeirdSettingsSection.cs
@@ -61,7 +61,7 @@ namespace CompatBot.Utils.ResultFormatters
                 if (rsxCaveats.Contains("alpha-to-one for multisampling"))
                 {
                     if (items["msaa"] is string msaa && msaa != "Disabled")
-                        generalNotes.Add("⚠ The drivers or GPU do not support all required features for proper MSAA implementation");
+                        generalNotes.Add("⚠ The driver or GPU do not support all required features for proper MSAA implementation");
                 }
             }
             var isWireframeBugPossible = items["gpu_info"] is string gpuInfo
