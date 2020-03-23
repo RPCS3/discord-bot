1ca9dd05a1ef64e375564455a4c17a0f20020301
xddxd <dsdominykas@gmail.com>
2020-03-24 00:45:14 +0200

    Fix the build monitor (#553)
    * Fix the build monitor
    
    * Update NewBuildsMonitor.cs
    
    * Update NewBuildsMonitor.cs
    
    * Update NewBuildsMonitor.cs
    
    * Update NewBuildsMonitor.cs
    
    * Update NewBuildsMonitor.cs
    
    * Update NewBuildsMonitor.cs
    
    * Update NewBuildsMonitor.cs
    
    * Update NewBuildsMonitor.cs

--- CompatBot/EventHandlers/NewBuildsMonitor.cs
+++ CompatBot/EventHandlers/NewBuildsMonitor.cs
@@ -43,12 +43,15 @@ namespace CompatBot.EventHandlers
                         if (DateTime.UtcNow - lastCheck > CheckInterval)
                         {
                             await CompatList.UpdatesCheck.CheckForRpcs3Updates(client, null).ConfigureAwait(false);
+                            lastCheck = DateTime.UtcNow;
                             if (DateTime.UtcNow - resetThreshold > RapidStart)
                                 Reset();
                         }
                     }
-                    catch { }
-                    lastCheck = DateTime.UtcNow;
+                    catch
+                        {
+                            lastCheck = DateTime.UtcNow;
+                        }
                     await Task.Delay(1000, Config.Cts.Token).ConfigureAwait(false);
                 }
             }
