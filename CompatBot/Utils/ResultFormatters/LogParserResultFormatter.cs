26b1e39e4a561d6ea98e01ed846b8fb853c22d04
13xforever <ilya.veselov@gmail.com>
2020-03-24 00:01:47 +0500

    tweak fatal error grouping slightly to show different ppu thread names

--- CompatBot/Utils/ResultFormatters/LogParserResultFormatter.cs
+++ CompatBot/Utils/ResultFormatters/LogParserResultFormatter.cs
@@ -801,7 +801,7 @@ namespace CompatBot.Utils.ResultFormatters
                 for (var i = 0; i < result.Count; i++)
                 {
                     similarity = result[i].fatalError.GetFuzzyCoefficientCached(error);
-                    if (similarity > 0.95)
+                    if (similarity > 0.96) // spu worker X gives .9763 confidence, ppu thread XXXX gives about .9525
                     {
                         idx = i;
                         break;
