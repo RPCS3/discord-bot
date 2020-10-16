321751206529f9bbde5ad6d4f9e9281e7081cbb8
13xforever <ilya.veselov@gmail.com>
2020-10-17 03:35:44 +0500

    !psn add

--- CompatBot/Commands/Psn.cs
+++ CompatBot/Commands/Psn.cs
@@ -6,6 +6,7 @@ using CompatBot.Commands.Attributes;
 using CompatBot.Database;
 using CompatBot.Database.Providers;
 using CompatBot.EventHandlers;
+using CompatBot.ThumbScrapper;
 using CompatBot.Utils;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -90,6 +91,47 @@ namespace CompatBot.Commands
                 item.Name = title;
                 await db.SaveChangesAsync().ConfigureAwait(false);
                 await ctx.ReactWithAsync(Config.Reactions.Success, "Title updated successfully").ConfigureAwait(false);
+            }
+        }
+
+        [Command("add"), RequiresBotModRole]
+        [Description("Add new product code with specified title to the bot database")]
+        public async Task Add(CommandContext ctx, [Description("Product code such as BLUS12345")] string contentId, [RemainingText, Description("New game title to save in the database")] string title)
+        {
+            contentId = contentId.ToUpperInvariant();
+            var productCode = contentId;
+            var productCodeMatch = ProductCodeLookup.ProductCode.Match(contentId);
+            var contentIdMatch = PsnScraper.ContentIdMatcher.Match(contentId);
+            if (contentIdMatch.Success)
+            {
+                productCode = contentIdMatch.Groups["product_id"].Value;
+            }
+            else if (productCodeMatch.Success)
+            {
+                productCode = productCodeMatch.Groups["letters"].Value + productCodeMatch.Groups["numbers"].Value;
+                contentId = null;
+            }
+            else
+            {
+                await ctx.ReactWithAsync(Config.Reactions.Failure, "Invalid content id", true).ConfigureAwait(false);
+                return;
+            }
+            
+            using var db = new ThumbnailDb();
+            var item = db.Thumbnail.FirstOrDefault(t => t.ProductCode == productCode);
+            if (item != null) 
+                await ctx.ReactWithAsync(Config.Reactions.Failure, $"Product code {contentId} already exists", true).ConfigureAwait(false);
+            else
+            {
+                item = new Thumbnail
+                {
+                    ProductCode = contentId,
+                    ContentId = contentId,
+                    Name = title,
+                };
+                db.Add(item);
+                await db.SaveChangesAsync().ConfigureAwait(false);
+                await ctx.ReactWithAsync(Config.Reactions.Success, "Title added successfully").ConfigureAwait(false);
             }
         }
 
