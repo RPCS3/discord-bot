97ea398d563ac7b8fd74f540a59bb0e282e9e38f
13xforever <ilya.veselov@gmail.com>
2020-11-11 01:31:22 +0500

    update homoglyph library for new c# language features

--- HomoglyphConverter/ConfusablesBuilder.cs
+++ HomoglyphConverter/ConfusablesBuilder.cs
@@ -20,32 +20,35 @@ namespace HomoglyphConverter
         {
             var result = new Dictionary<uint, uint[]>();
             var assembly = Assembly.GetAssembly(typeof(ConfusablesBuilder));
-            var resourceName = assembly.GetManifestResourceNames().FirstOrDefault(n => n.EndsWith("confusables.txt.gz", StringComparison.InvariantCultureIgnoreCase));
-            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            var resourceName = assembly?.GetManifestResourceNames().FirstOrDefault(n => n.EndsWith("confusables.txt.gz", StringComparison.InvariantCultureIgnoreCase));
+            if (string.IsNullOrEmpty(resourceName))
+                throw new InvalidOperationException("Confusables embedded resource was not found");
+            
+            using var stream = assembly?.GetManifestResourceStream(resourceName);
+            if (stream is null)
+                throw new InvalidOperationException("Failed to get confusables resource stream");
+            
+            using var gzip = new GZipStream(stream, CompressionMode.Decompress);
+            using var reader = new StreamReader(gzip, Encoding.UTF8, false);
+            while (reader.ReadLine() is string line)
             {
-                using var gzip = new GZipStream(stream, CompressionMode.Decompress);
-                using var reader = new StreamReader(gzip, Encoding.UTF8, false);
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                if (string.IsNullOrEmpty(line) || line.StartsWith("#"))
+                    continue;
+
+                var lineParts = line.Split(CommentSplitter, 2);
+                var mapping = lineParts[0].Split(FieldSplitter, 3);
+                if (mapping.Length < 2)
+                    throw new InvalidOperationException("Invalid confusable mapping line: " + line);
+
+                try
                 {
-                    if (string.IsNullOrEmpty(line) || line.StartsWith("#"))
-                        continue;
-
-                    var lineParts = line.Split(CommentSplitter, 2);
-                    var mapping = lineParts[0].Split(FieldSplitter, 3);
-                    if (mapping.Length < 2)
-                        throw new InvalidOperationException("Invalid confusable mapping line: " + line);
-
-                    try
-                    {
-                        var confusableChar = uint.Parse(mapping[0].Trim(), NumberStyles.HexNumber);
-                        var skeletonChars = mapping[1].Split(PairSplitter, StringSplitOptions.RemoveEmptyEntries).Select(l => uint.Parse(l, NumberStyles.HexNumber)).ToArray();
-                        result.Add(confusableChar, skeletonChars);
-                    }
-                    catch (Exception e)
-                    {
-                        throw new InvalidOperationException("Invalid confusable mapping line:" + line, e);
-                    }
+                    var confusableChar = uint.Parse(mapping[0].Trim(), NumberStyles.HexNumber);
+                    var skeletonChars = mapping[1].Split(PairSplitter, StringSplitOptions.RemoveEmptyEntries).Select(l => uint.Parse(l, NumberStyles.HexNumber)).ToArray();
+                    result.Add(confusableChar, skeletonChars);
+                }
+                catch (Exception e)
+                {
+                    throw new InvalidOperationException("Invalid confusable mapping line:" + line, e);
                 }
             }
             if (result.Count == 0)
