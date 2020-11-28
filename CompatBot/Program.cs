33b73ff00fb05b910ff0a5bbe2d99f6d2bba7c12
13xforever <ilya.veselov@gmail.com>
2020-11-28 17:40:11 +0500

    disable minesweeper as discord broke it beyond being useful

--- CompatBot/Program.cs
+++ CompatBot/Program.cs
@@ -161,7 +161,7 @@ namespace CompatBot
                 commands.RegisterCommands<BotStats>();
                 commands.RegisterCommands<Syscall>();
                 commands.RegisterCommands<ForcedNicknames>();
-                commands.RegisterCommands<Minesweeper>();
+                //commands.RegisterCommands<Minesweeper>();
 
                 if (!string.IsNullOrEmpty(Config.AzureComputerVisionKey))
                     commands.RegisterCommands<Vision>();
