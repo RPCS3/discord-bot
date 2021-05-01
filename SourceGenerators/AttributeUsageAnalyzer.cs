a0b2113b40a26cc3e19d4b0eaf0de45095e2223f
13xforever <ilya.veselov@gmail.com>
2021-05-01 20:17:41 +0500

    change analyzer diagnostics id

--- SourceGenerators/AttributeUsageAnalyzer.cs
+++ SourceGenerators/AttributeUsageAnalyzer.cs
@@ -10,7 +10,7 @@ namespace SourceGenerators
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private const string Category = "Usage";
-        private const string DiagnosticId = "DSharpPlusCodeChecks";
+        private const string DiagnosticId = "DSharpPlusAttributeUsage";
 
         private static readonly DiagnosticDescriptor AccessCheckAttributeOnGroupCommandRule = new DiagnosticDescriptor(
             DiagnosticId,
