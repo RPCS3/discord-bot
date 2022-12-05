7e87283f1ebf0e284bafebc09cf9c19e555ff22b
13xforever <ilya.veselov@gmail.com>
2022-12-05 12:54:06 +0500

    update win32 error codes source handling

--- SourceGenerators/Win32ErrorsSourceGenerator.cs
+++ SourceGenerators/Win32ErrorsSourceGenerator.cs
@@ -29,13 +29,15 @@ public class Win32ErrorsSourceGenerator : ISourceGenerator
 
     public void Execute(GeneratorExecutionContext context)
     {
-        var resourceName = context.AdditionalFiles.FirstOrDefault(f => Path.GetFileName(f.Path).Equals("win32_error_codes.txt"));
+        var resourceName = context.AdditionalFiles
+            .OrderBy(f => f.Path, StringComparer.OrdinalIgnoreCase)
+            .LastOrDefault(f => Path.GetFileName(f.Path) is string fname && fname.StartsWith("win32_error_codes") && fname.EndsWith(".txt"));
         if (resourceName is null)
-            return;
+            throw new InvalidOperationException("Failed to locate win32_error_codes txt file");
             
         using var stream = File.Open(resourceName.Path, FileMode.Open, FileAccess.Read, FileShare.Read);
         if (stream is null)
-            throw new InvalidOperationException("Failed to get win32_error_codes.txt stream");
+            throw new InvalidOperationException("Failed to get win32_error_codes txt stream");
 
         if (!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.RootNamespace", out var ns))
             ns = context.Compilation.AssemblyName;
