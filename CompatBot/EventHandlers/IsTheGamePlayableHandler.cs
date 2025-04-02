14bc3388ab164363a43128898babb3a344eea616
13xforever <ilya.veselov@gmail.com>
2025-04-02 14:19:37 +0500

    fix help message to use the new command syntax

--- CompatBot/EventHandlers/IsTheGamePlayableHandler.cs
+++ CompatBot/EventHandlers/IsTheGamePlayableHandler.cs
@@ -81,7 +81,7 @@ internal static partial class IsTheGamePlayableHandler
                 if (!string.IsNullOrEmpty(info.Date))
                     msg += $" since {info.ToUpdated()}";
             }
-            msg += $"\nfor more results please use [compatibility list](<https://rpcs3.net/compatibility>) or `{Config.CommandPrefix}c` command in {botSpamChannel.Mention} (`!c {gameTitle.Sanitize()}`)";
+            msg += $"\nfor more results please use [compatibility list](<https://rpcs3.net/compatibility>) or `/compatibility title:{gameTitle.Sanitize()}` command in {botSpamChannel.Mention}";
             await args.Channel.SendMessageAsync(msg).ConfigureAwait(false);
             CooldownBuckets[args.Channel.Id] = DateTime.UtcNow;
         }
