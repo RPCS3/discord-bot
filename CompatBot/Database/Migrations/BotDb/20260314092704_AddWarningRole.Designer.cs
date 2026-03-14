8e841ec51403fa3c52a4ca3b68e2dff0175280dd
13xforever <ilya.veselov@gmail.com>
2026-03-14 14:29:23 +0500

    add new table for user ids with role

--- CompatBot/Database/Migrations/BotDb/20260314092704_AddWarningRole.Designer.cs
+++ CompatBot/Database/Migrations/BotDb/20260314092704_AddWarningRole.Designer.cs
@@ -2,6 +2,7 @@
 using CompatBot.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CompatBot.Database.Migrations
 {
     [DbContext(typeof(BotDb))]
-    partial class BotDbModelSnapshot : ModelSnapshot
+    [Migration("20260314092704_AddWarningRole")]
+    partial class AddWarningRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
---
+++ CompatBot/Database/Migrations/BotDb/20260314092704_AddWarningRole.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompatBot.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWarningRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "forced_warning_roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    user_id = table.Column<ulong>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("id", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "forced_warning_role_user_id",
+                table: "forced_warning_roles",
+                column: "user_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "forced_warning_roles");
+        }
+    }
+}
