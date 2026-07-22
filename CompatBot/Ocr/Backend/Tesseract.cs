ab7931258e85558419d40cb53d28b1790de5ea71
13xforever <ilya.veselov@gmail.com>
2026-07-22 16:23:51 +0500

    update tesseract ocr backend

--- CompatBot/Ocr/Backend/Tesseract.cs
+++ CompatBot/Ocr/Backend/Tesseract.cs
@@ -8,8 +8,8 @@ namespace CompatBot.Ocr.Backend;
 
 internal class Tesseract: BackendBase
 {
-    private TesseractEngine engine;
-    private static readonly SemaphoreSlim limiter = new(1, 1);
+    private TesseractEngine engine = null!;
+    private static readonly SemaphoreSlim Limiter = new(1, 1);
 
     public override string Name => "tesseract";
 
@@ -28,34 +28,14 @@ internal class Tesseract: BackendBase
             return false;
         }
 
-        var engModelPath = Path.Combine(ModelCachePath, "eng.traineddata");
-        if (!File.Exists(engModelPath))
-        {
-            try
-            {
-                using var client = HttpClientFactory.Create(new CompressionMessageHandler());
-                // existing repos: tessdata_fast, tessdata, tessdata_best
-                const string uri = "https://github.com/tesseract-ocr/tessdata_best/raw/refs/heads/main/eng.traineddata";
-                await using var response = await client.GetStreamAsync(uri, cancellationToken).ConfigureAwait(false);
-                await using var file = File.Open(engModelPath, new FileStreamOptions
-                {
-                    Mode = FileMode.Create,
-                    Access = FileAccess.Write,
-                    Share = FileShare.None,
-                    Options = FileOptions.Asynchronous | FileOptions.SequentialScan,
-                });
-                await response.CopyToAsync(file, cancellationToken).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                Config.Log.Error(e, "Failed to download model data");
-                return false;
-            }
-        }
+        var modelsAvailable = await EnsureModelIsCached("eng", cancellationToken).ConfigureAwait(false)
+                              && await EnsureModelIsCached("rus", cancellationToken).ConfigureAwait(false);
+        if (!modelsAvailable)
+            return false;
 
         try
         {
-            engine = new(ModelCachePath, "eng", EngineMode.Default);
+            engine = new(ModelCachePath, "eng+rus", EngineMode.Default);
         }
         catch (Exception e)
         {
@@ -84,7 +64,7 @@ internal class Tesseract: BackendBase
                 img.Dispose();
                 img = img2;
             }
-            await limiter.WaitAsync(Config.Cts.Token).ConfigureAwait(false);
+            await Limiter.WaitAsync(Config.Cts.Token).ConfigureAwait(false);
             try
             {
                 using var page = engine.Process(img);
@@ -92,7 +72,7 @@ internal class Tesseract: BackendBase
             }
             finally
             {
-                limiter.Release();
+                Limiter.Release();
             }
         }
         finally
@@ -105,6 +85,35 @@ internal class Tesseract: BackendBase
     {
         base.Dispose();
         engine.Dispose();
+    }
+
+    private async ValueTask<bool> EnsureModelIsCached(string lang, CancellationToken cancellationToken)
+    {
+        var modelPath = Path.Combine(ModelCachePath, $"{lang}.traineddata");
+        if (File.Exists(modelPath))
+            return true;
+        
+        try
+        {
+            using var client = HttpClientFactory.Create(new CompressionMessageHandler());
+            // existing repos: tessdata_fast, tessdata, tessdata_best
+            var uri = $"https://github.com/tesseract-ocr/tessdata_best/raw/refs/heads/main/{lang}.traineddata";
+            await using var response = await client.GetStreamAsync(uri, cancellationToken).ConfigureAwait(false);
+            await using var file = File.Open(modelPath, new FileStreamOptions
+            {
+                Mode = FileMode.Create,
+                Access = FileAccess.Write,
+                Share = FileShare.None,
+                Options = FileOptions.Asynchronous | FileOptions.SequentialScan,
+            });
+            await response.CopyToAsync(file, cancellationToken).ConfigureAwait(false);
+        }
+        catch (Exception e)
+        {
+            Config.Log.Error(e, $"Failed to download model data for language {lang}");
+            return false;
+        }
+        return true;
     }
 
     private enum RotationDirection
